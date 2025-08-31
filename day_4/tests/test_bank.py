e71f56a501c60e7a2c51ea8a9cb72d2568f95192
Dastan <ddbolatkhan@gmail.com>
2025-08-31 17:25:50 +0500

    pre last commit

--- day_4/tests/test_bank.py
+++ day_4/tests/test_bank.py
@@ -12,8 +12,10 @@ def other_account():
 
 def test_deposit_increase(my_account):
     # Тест на пополнение аккаунта
-    my_account.deposit(5000)
-    assert my_account._balance == 15000
+    deposit_amount = 5000
+    before_balance = my_account._balance
+    my_account.deposit(deposit_amount)
+    assert my_account._balance == before_balance + deposit_amount
 
 def test_deposit_negative(my_account):
     # Тест на поплнение отрицательной суммы
@@ -22,12 +24,26 @@ def test_deposit_negative(my_account):
 
 def test_withdraw_decrease(my_account):
     # Тест на снятие денег
+    withdraw_amount = 999
     before_withdraw_balance = my_account._balance
-    my_account.withdraw(999)
-    assert my_account._balance == (before_withdraw_balance - 999)
+    my_account.withdraw(withdraw_amount)
+    assert my_account._balance == (before_withdraw_balance - withdraw_amount)
 
 def test_withdraw_error(my_account):
     # Тест на снятие денег больше чем имеется на счету
     withdraw_bigger = my_account._balance + 10
     with pytest.raises(InsufficientFundsError):
         my_account.withdraw(withdraw_bigger)
+
+def test_transfer(my_account, other_account):
+    # Тест на перевод денег на другой аккаунт
+    transfer_amount = 5000
+    before_other_acount = other_account._balance
+    my_account.transfer(transfer_amount, other_account)
+    assert other_account._balance == before_other_acount + transfer_amount
+
+def test_transfer_error(my_account, other_account):
+    # Тест на перевод денег больше чем имеется на счету
+    transfer_bigger = my_account._balance + 10
+    with pytest.raises(InsufficientFundsError):
+        my_account.transfer(transfer_bigger, other_account)
